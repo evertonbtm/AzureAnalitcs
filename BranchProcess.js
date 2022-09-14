012155c609191d18cf7650c8235f05d96e66648a
Everton Batista <everton.batista@totvs.com.br>
2022-09-14 14:37:05 -0300

    commit melhorias

--- BranchProcess.js
+++ BranchProcess.js
@@ -46,23 +46,15 @@ const capitalizeRepos = async () => {
 function promisseProcess(promiseAll){
 	let processed = [];
 	
-	let headers = {
-		"repositorie" : "Repositorio",
-		"url" : "Url",
-		"size" : "Tamanho",
-		"branch" : "Branch",
-		"coreVersion": "Versão core",
-		"clienteVersion": "Versão cliente",
-		"isBaseVersion": "Branch base",
-		//"ref" : "Referência",		
-		"repoLastUpdate" : "Repositorio Atualizado",
-		"lastCommit" : "Ultimo commit",
-		"lastBuild" : "Ultima pipeline",
-		"commitsAhead":  "Commits à frente",
-		"commitsBehind": "Commits atras"
-	}
-	
-	processed.push(headers);
+	params.forEach(arg => {
+		if(arg.includes('branch')){
+			processed = processed.concat(branchHeader());
+		}else if(arg.includes('version')){
+			processed = processed.concat(versionHeader());
+		}else{
+			processed = processed.concat(branchHeader());
+		}
+	});
 	
 	promiseAll.forEach(promise => {
 		if(promise.status == 'fulfilled'){
@@ -74,14 +66,21 @@ function promisseProcess(promiseAll){
 		}
 	});
 	
+	const processedSorted = Object.keys(processed).map(function (key) {
+	  return processed[key];
+	})
+	.sort(function (itemA, itemB) {
+	  return itemA.repositorie < itemB.repositorie;
+	});
+
+	
 	params.forEach(arg => {
-		miscUtils.toWorkbook(processed);
-		
 		if(arg === 'planilha'){
-			miscUtils.toWorkbook(processed);
-		}		
-		if(arg === 'txt'){
+			miscUtils.toWorkbook(processedSorted);
+		}else if(arg === 'txt'){
 			
+		}else{		
+			miscUtils.toWorkbook(processedSorted);			
 		}
 	});
 }
@@ -92,7 +91,63 @@ capitalizeRepos();
 function processResults(repositorie, branchs, stats, xmlVersion){
 	
 	let version = miscUtils.getXmlVersion(xmlVersion);
+
+	let processed = [];
+	params.forEach(arg => {
+		if(arg.includes('branch')){
+			processed = branchAnalitcs(repositorie, branchs, stats);
+		}else if(arg.includes('version')){
+			processed = versionAnalitcs(repositorie, version);
+		}else{
+			processed = branchAnalitcs(repositorie, branchs, stats);
+		}
+	});
 	
+	return processed;
+}
+
+function versionHeader(){
+	let processed = [];
+	
+	let headers = {
+		"repositorie" : "Repositorio",
+		"url" : "Url",
+		"coreVersion" : "Versão core",
+		"clienteVersion": "Versão cliente",
+		"size" : "Tamanho",
+		//"ref" : "Referência",		
+		"repoLastUpdate" : "Repositorio Atualizado"
+	}
+	
+	processed.push(headers);
+	
+	return processed;
+}
+
+function branchHeader(){
+	let processed = [];
+	
+	let headers = {
+		"repositorie" : "Repositorio",
+		"url" : "Url",
+		"size" : "Tamanho",
+		"branch" : "Branch",
+		"isBaseVersion": "Branch base",
+		//"ref" : "Referência",		
+		"repoLastUpdate" : "Repositorio Atualizado",
+		"lastCommit" : "Ultimo commit",
+		"lastBuild" : "Ultima pipeline",
+		"commitsAhead":  "Commits à frente",
+		"commitsBehind": "Commits atras"
+	}
+	
+	processed.push(headers);
+	
+	return processed;
+}
+
+function versionAnalitcs(repositorie, version){
+	let processed = [];	
 	let customer = undefined;
 	
 	params.forEach(arg => {
@@ -100,8 +155,39 @@ function processResults(repositorie, branchs, stats, xmlVersion){
 			customer= arg.replace('cliente=','');
 		}
 	});
+
+	if( (customer && !repositorie.name.toUpperCase().includes(customer.toUpperCase())) || 
+		!repositorie.name.includes('SFA-CLIENTE') && !repositorie.name.includes('SFA-LEGACY-Sim3g')){
+		//exclude
+		return;
+	}
+
+	let row = {
+		"repositorie" : repositorie.name,
+		"url" : repositorie.remoteUrl,		
+		"coreVersion": (version != null ? version['core.version']._text : "" ),
+		"clienteVersion": (version != null ? version['cliente.version']._text : "" ),
+		"size" : miscUtils.formatBytes(repositorie.size, 2),
+		//"ref" : ( branch != null ? branch.name : ""),			
+		"repoLastUpdate" : miscUtils.formatDate(repositorie.project.lastUpdateTime)
+	};
 	
+	processed.push(row);
+
+	
+	return processed;
+}
+
+function branchAnalitcs(repositorie, branchs, stats){
 	let processed = [];
+	let customer = undefined;
+	
+	params.forEach(arg => {
+		if(arg.includes('cliente')){
+			customer= arg.replace('cliente=','');
+		}
+	});
+
 	branchs.forEach(branch => {	
 		if( (customer && !repositorie.name.toUpperCase().includes(customer.toUpperCase())) || 
 			!repositorie.name.includes('SFA-CLIENTE') && !repositorie.name.includes('SFA-LEGACY-Sim3g')){
@@ -126,8 +212,6 @@ function processResults(repositorie, branchs, stats, xmlVersion){
 			"url" : repositorie.remoteUrl,
 			"size" : miscUtils.formatBytes(repositorie.size, 2),
 			"branch" : ( stat != null ? stat.name : ""),
-			"coreVersion": (version != null && stat.name == 'master' ? version['core.version']._text : "" ),
-			"clienteVersion": (version != null && stat.name == 'master' ? version['cliente.version']._text : "" ),
 			"isBaseVersion": ( stat != null ? String(stat.isBaseVersion) : "sem dados"),
 			//"ref" : ( branch != null ? branch.name : ""),			
 			"repoLastUpdate" : miscUtils.formatDate(repositorie.project.lastUpdateTime),
